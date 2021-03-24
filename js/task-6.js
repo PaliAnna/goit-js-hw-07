92a7448220f4b53b90d949a8c3c33db307b3fa61
PaliAnna <72465798+PaliAnna@users.noreply.github.com>
2021-03-24 06:47:12 +0200

    Внесення випралень

--- js/task-6.js
+++ js/task-6.js
@@ -5,10 +5,10 @@ const validationCheck = event => {
   const currentElRef = event.target;
 
   if (currentElRef.value.length !== Number(dataLengthValue)) {
-    currentElRef.classList.add('invalid');
+   
     currentElRef.classList.remove('valid');
   } else {
-    currentElRef.classList.add('valid');
+   
     currentElRef.classList.remove('invalid');
   }
 };
